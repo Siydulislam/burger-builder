a79e5e425db48f00563af90e7dc61cd4f30eca06
Siydulislam <siydulislam333@gmail.com>
2020-12-22 16:55:31 +0600

    order list done

---
+++ src/Components/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
+import Header from './Header/Header';
+import { Route } from 'react-router-dom';
+import Orders from './Orders/Orders';
+import Checkout from './Orders/Checkout/Checkout';
+
+const Main = props => {
+    return (
+        <div>
+            <Header></Header>
+            <div className="container">
+                <Route path="/orders" component={Orders}></Route>
+                <Route path="/checkout" component={Checkout}></Route>
+                <Route path="/" exact component={BurgerBuilder}></Route>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
